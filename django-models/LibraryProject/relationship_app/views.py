47ea496fe7403cf334208c068720c664920c693a
stephen842 <ugotachisomstephen@gmail.com>
2025-11-09 13:47:16 +0100

    hotfix

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,6 +31,7 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+
 @permission_required('relationship_app.can_add_book')
 def add_book(request):
     return render(request, 'relationship_app/add_book.html')
@@ -82,28 +83,24 @@ def logout_view(request):
     logout(request)
     return render(request, 'relationship_app/logout.html')
 
-# Helper checks
 def is_admin(user):
     return user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
-
 
 @login_required
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
 
 @login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
+def is_member(user):
+    return user.userprofile.role == 'Member'
 
 @login_required
 @user_passes_test(is_member)
