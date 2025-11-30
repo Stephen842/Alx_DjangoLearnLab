b8c5cc401c235c83278e26c0c120a6a9353f0b36
stephen842 <ugotachisomstephen@gmail.com>
2025-11-30 02:36:07 +0100

    htfixing some typo

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,7 +3,6 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import Book
 from .serializers import BookSerializer
@@ -20,7 +19,7 @@ class BookListView(generics.ListAPIView):
     permissions_classes = [IsAuthenticatedOrReadOnly]
 
     # Enable filtering, searching, ordering
-    filter_backends = [DjangoFilterBackend, SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
 
     # Filter by fields
     filterset_fields = ['title', 'publication_year', 'author']
