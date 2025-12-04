9330f9f6449bce089c7b0195231bf30851baf78f
stephen842 <ugotachisomstephen@gmail.com>
2025-12-04 20:50:42 +0100

    hotfix the url to meet checkers requirement

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     ### Blog Comment routing
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
-    path('comments/<int:pk>/edit/', CommentUpdateView.as_view(), name='comment-update'),
-    path('comments/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
 ]
