c56353a014db1dcf75869ca23bb16097af8b7a48
stephen842 <ugotachisomstephen@gmail.com>
2025-12-20 10:12:35 +0100

    Add posts and comments CRUD with permissions, pagination, and filtering

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Allow read-only requests
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions only to the author
+        return obj.author == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    #  Filtering & Searching
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['author']
+    search_fields = ['title', 'content']
+    ordering_fields = ['created_at']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['post', 'author']
+    ordering_fields = ['created_at']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
